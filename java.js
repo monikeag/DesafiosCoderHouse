c524b6a66c3c3a2747a2b0d7d72180791a1b57e6
Monique Aguiar <monikeagn@gmail.com>
2023-10-30 20:34:30 -0300

    novo

--- java.js
+++ java.js
@@ -1,1 +1,51 @@
-alert('Aprendendo Java')
+/*let nome = document.createElement("h1");
+
+nome.innerText = "Monique"
+
+document.body.append(nome);
+
+
+let sobrenome = document.getElementById("h2");
+sobrenome.remove();*/
+function main()
+{
+ var resp = document.getElementById('resp');
+ var num1 = parseFloat(document.getElementById("num1").value);
+ var num2 = parseFloat(document.getElementById("num2").value);
+ var texto='';
+
+ if(document.getElementById('btnsoma').checked)
+  texto = soma(num1, num2);
+ if(document.getElementById('btnsubtracao').checked)
+  texto = subtracao(num1, num2);
+ if(document.getElementById('btnmultiplicacao').checked)
+  texto = multiplicacao(num1, num2);
+ if(document.getElementById('btndivisao').checked)
+  texto = divisao(num1, num2);
+
+ resp.innerHTML = texto;
+}
+
+function soma(x, y)
+{
+  
+  return (x+y);
+}
+
+function subtracao(x, y)
+{
+  
+  return (x-y);
+}
+
+function multiplicacao(x, y)
+{
+  
+  return (x*y);
+}
+
+function divisao(x, y)
+{
+  
+  return (x/y);
+}
