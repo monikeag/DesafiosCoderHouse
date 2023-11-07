c2b4af56f2ba55ef32bca91bba49c823a90f07b0
Monique Aguiar <monikeagn@gmail.com>
2023-11-07 15:45:00 -0300

    java

--- java.js
+++ java.js
@@ -1,4 +1,22 @@
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(response => console.log(response))
+function buscarBandeira() {
+  
+   let paisDigitado = document.getElementById("paisDigitado")
 
+   let url = 'https://restcountries.com/v3.1/name/' + paisDigitado.value;
+
+   fetch(url)
+       .then(response => response.json())
+       .then(response => exibirPais(response))
+}
+
+function exibirPais(pais) {
+
+   let divImg = document.getElementById("bandeira")
+
+   divImg.innerText = ""
+
+   let bandeira = document.createElement("img")
+   bandeira.src = pais[0].flags.png
+
+   divImg.append(bandeira)
+}
