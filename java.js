d1e22b65d5307c69ec3d25720189c12ab2eb865e
Monique Aguiar <monikeagn@gmail.com>
2023-11-07 10:04:10 -0300

    alteração

--- java.js
+++ java.js
@@ -7,8 +7,8 @@ document.body.append(nome);
 
 let sobrenome = document.getElementById("h2");
 sobrenome.remove();*/
-function main()
-{
+
+
  var resp = document.getElementById('resp');
  var num1 = parseFloat(document.getElementById("num1").value);
  var num2 = parseFloat(document.getElementById("num2").value);
@@ -23,29 +23,9 @@ function main()
  if(document.getElementById('btndivisao').checked)
   texto = divisao(num1, num2);
 
- resp.innerHTML = texto;
-}
+ let resp = document.getElementById("resp");
 
-function soma(x, y)
-{
-  
-  return (x+y);
-}
-
-function subtracao(x, y)
-{
-  
-  return (x-y);
-}
-
-function multiplicacao(x, y)
-{
-  
-  return (x*y);
-}
-
-function divisao(x, y)
-{
-  
-  return (x/y);
-}
+ function soma(){
+    let respSoma = parseFloat(num1.value) + parseFloat(nun2.value);
+    resp.innerText = respSoma;
+ };
